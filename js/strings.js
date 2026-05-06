5c77ea8dd372da6ca47ad1d21a2b64c981c30689
Meli <ms.melcastro@gmail.com>
2026-05-06 13:47:11 -0700

    step 1 & 2

--- js/strings.js
+++ js/strings.js
@@ -1,19 +1,31 @@
 //STEP 1
+// let name1 = prompt("Enter your name:")
+// alert("Your name has " + name1.length + " characters.")
 
 //STEP 2
+// let name2 = prompt("Enter your name:")
+// let index = prompt("Enter a number:")
+// alert("The letter is: " + name2.charAt(index))
 
 //STEP 3
 
+
 //STEP 4
+
 
 //STEP 5
 
+
 //STEP 6
+
 
 //STEP 7
 
+
 //STEP 8
 
+
 //STEP 9
+
 
 //STEP 10
