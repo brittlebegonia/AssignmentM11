aee1a73f824098b00fbdd0d6477fd6b47cb9f13f
Meli <ms.melcastro@gmail.com>
2026-05-06 14:27:26 -0700

    math

--- js/math.js
+++ js/math.js
@@ -1,9 +1,23 @@
 //STEP 1
+// let num1 = prompt("Enter a positive or negative number:")
+// console.log(Math.abs(num1))
 
 //STEP 2
+// let num2 = prompt("Enter a decimal number:")
+// console.log(Math.ceil(num2))
 
 //STEP 3
+// let num3 = prompt("Enter a decimal number:")
+// console.log(Math.floor(num3))
 
 //STEP 4
+// let numbers = prompt("Enter 5 numbers separated by commas:")
+
+// let numArray = numbers.split(",")
+
+// console.log("Largest number: " + Math.max(...numArray))
+// console.log("Smallest number: " + Math.min(...numArray))
 
 //STEP 5
+// let num4 = prompt("Enter a number:")
+// console.log(Math.sqrt(num4))
