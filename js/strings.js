2f6100eba950c42ea3ac9212d6d73637764fbab7
Meli <ms.melcastro@gmail.com>
2026-05-06 14:05:38 -0700

    next steps

--- js/strings.js
+++ js/strings.js
@@ -21,15 +21,27 @@
 // alert("Last index of 'fox': " + text2.lastIndexOf("fox"))
 
 //STEP 6
-
+// let text3 = "The quick brown fox jumped over the lazy dog"
+// let fullName = prompt("Enter your full name:")
+// let newText = text3.replace("the lazy dog", fullName)
+// alert(newText)
 
 //STEP 7
-
+// let text4 = "The quick brown fox jumps over the lazy dog"
+// let word = prompt("Enter a word:")
+// alert(text4.indexOf(word))
 
 //STEP 8
-
+// let old_string = "The quick brown fox jumps over the lazy dog"
+// let new_string = old_string.slice(-12)
+// alert(new_string.toUpperCase())
 
 //STEP 9
-
+// let text5 = " THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          "
+// let result1 = text5.trim().toLowerCase()
+// alert(result1)
 
 //STEP 10
+// let text6 = "the quick brown fox jumps over the lazy dog"
+// let result2 = text6.charAt(0).toUpperCase() + text6.slice(1)
+// alert(result2)
